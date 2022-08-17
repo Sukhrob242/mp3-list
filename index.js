35b23769dbb1eb720ca5bface71995d06bc846dd
Sukhrobjon <79089497+Sukhrob242@users.noreply.github.com>
2022-08-17 16:31:38 +0500

    js file
    add new js file

---
+++ index.js
@@ -0,0 +1,10 @@
+const musicContainer = document.getElementById('audio-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progressContainer = document.getElementById('progress_box');
+const progress = document.getElementById('progress');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
